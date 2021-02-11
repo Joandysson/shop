a7a2f7573c66c4c2120414cac7003fc2682e6ce5
Joandysson Gama <joandysson.gama@gmail.com>
2021-02-11 14:58:06 -0300

    added provider

--- lib/views/products_overview.dart
+++ lib/views/products_overview.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:shop/data/dummy_data.dart';
-import 'package:shop/models/product.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/products.dart';
 import 'package:shop/widgets/product_item.dart';
 
 class ProductOverView extends StatelessWidget {
-  final List<Product> loadProducts = DUMMY_PRODUCT;
-
   @override
   Widget build(BuildContext context) {
+    final productsProvider = Provider.of<Products>(context);
+    final products = productsProvider.items;
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha Loja'),
       ),
       body: GridView.builder(
         padding: const EdgeInsets.all(10),
-        itemCount: loadProducts.length,
-        itemBuilder: (ctx, i) => ProductItem(loadProducts[i]),
+        itemCount: products.length,
+        itemBuilder: (ctx, i) => ProductItem(products[i]),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
