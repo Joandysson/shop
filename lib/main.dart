ce8d072b5d5b2f5fbf7462555f46dc6896724480
Joandysson Gama <joandysson.gama@gmail.com>
2021-02-15 23:22:59 -0300

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,7 @@ import 'package:provider/provider.dart';
 import 'package:shop/providers/cart.dart' as CartProvider;
 import 'package:shop/providers/orders.dart' as OrderProvider;
 import 'package:shop/providers/products.dart' as ProductsProvider;
-
-import 'package:shop/views/oders.dart';
-import 'package:shop/views/product_detail.dart';
-import 'package:shop/views/product_form.dart';
-import 'package:shop/views/products.dart';
-import 'package:shop/views/products_overview.dart';
-import 'package:shop/views/cart.dart';
-
-import 'package:shop/utils/app_routes.dart';
+import 'package:shop/routes.dart';
 
 void main() => runApp(MyApp());
 
@@ -33,14 +25,7 @@ class MyApp extends StatelessWidget {
           fontFamily: 'Lato',
         ),
         // home: ProductOverView(),
-        routes: {
-          AppRoutes.HOME: (ctx) => ProductOverView(),
-          AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetail(),
-          AppRoutes.CART: (ctx) => Cart(),
-          AppRoutes.ORDERS: (ctx) => Orders(),
-          AppRoutes.PRODUCTS: (ctx) => Products(),
-          AppRoutes.PRODUCT_FORM: (ctx) => ProductForm(),
-        },
+        routes: routes,
         debugShowCheckedModeBanner: false,
       ),
     );
---
+++ lib/routes.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:shop/utils/app_routes.dart';
+import 'package:shop/views/cart.dart';
+import 'package:shop/views/oders.dart';
+import 'package:shop/views/product_detail.dart';
+import 'package:shop/views/product_form.dart';
+import 'package:shop/views/products.dart';
+import 'package:shop/views/products_overview.dart';
+
+final Map<String, Widget Function(BuildContext)> routes = {
+  AppRoutes.HOME: (ctx) => ProductOverView(),
+  AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetail(),
+  AppRoutes.CART: (ctx) => Cart(),
+  AppRoutes.ORDERS: (ctx) => Orders(),
+  AppRoutes.PRODUCTS: (ctx) => Products(),
+  AppRoutes.PRODUCT_FORM: (ctx) => ProductForm(),
+};
