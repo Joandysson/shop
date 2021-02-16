ee0ead355b5e7ae92912da35cd23a2f710847994
Joandysson Gama <joandysson.gama@gmail.com>
2021-02-15 23:06:57 -0300

    added FutureBuilder

--- lib/views/oders.dart
+++ lib/views/oders.dart
@@ -4,44 +4,51 @@ import 'package:shop/views/app_drower.dart';
 import 'package:shop/providers/orders.dart' as OrdersProvider;
 import 'package:shop/widgets/order.dart' as OrdersWidget;
 
-class Orders extends StatefulWidget {
-  @override
-  _OrdersState createState() => _OrdersState();
-}
-
-class _OrdersState extends State<Orders> {
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    Provider.of<OrdersProvider.Orders>(context, listen: false)
-        .loadOrders()
-        .then((_) {
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
-
+class Orders extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final OrdersProvider.Orders orders = Provider.of(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Meus pedidos'),
-      ),
-      drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: orders.itemsCount,
-              itemBuilder: (ctx, i) => OrdersWidget.Order(
-                orders.items[i],
-              ),
-            ),
-    );
+        appBar: AppBar(
+          title: Text('Meus pedidos'),
+        ),
+        drawer: AppDrawer(),
+        body: FutureBuilder(
+          future: Provider.of<OrdersProvider.Orders>(
+            context,
+            listen: false,
+          ).loadOrders(),
+          builder: (ctx, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            if (snapshot.error != null) {
+              return Center(child: Text('Error inesperado'));
+            }
+
+            return Consumer<OrdersProvider.Orders>(
+              builder: (ctx, orders, child) {
+                return ListView.builder(
+                  itemCount: orders.itemsCount,
+                  itemBuilder: (ctx, i) => OrdersWidget.Order(
+                    orders.items[i],
+                  ),
+                );
+              },
+            );
+          },
+        ));
+
+    //   _isLoading
+    //       ? Center(
+    //           child: CircularProgressIndicator(),
+    //         )
+    //       : ListView.builder(
+    //           itemCount: orders.itemsCount,
+    //           itemBuilder: (ctx, i) => OrdersWidget.Order(
+    //             orders.items[i],
+    //           ),
+    //         ),
+    // );
   }
 }
