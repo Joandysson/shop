b2aa2d96417be1fb19d5bc4323cf34b6b0fde5a8
Joandysson Gama <joandysson.gama@gmail.com>
2021-02-17 23:40:12 -0300

    screen login

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop/utils/app_routes.dart';
+import 'package:shop/views/auth.dart';
 import 'package:shop/views/cart.dart';
 import 'package:shop/views/oders.dart';
 import 'package:shop/views/product_detail.dart';
@@ -8,6 +9,7 @@ import 'package:shop/views/products.dart';
 import 'package:shop/views/products_overview.dart';
 
 final Map<String, Widget Function(BuildContext)> routes = {
+  AppRoutes.AUTH: (ctx) => Auth(),
   AppRoutes.HOME: (ctx) => ProductOverView(),
   AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetail(),
   AppRoutes.CART: (ctx) => Cart(),
